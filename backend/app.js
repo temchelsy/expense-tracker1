924b778fba52c20e8f22eb5e2962ed9282de91ef
temchelsy <148960005+temchelsy@users.noreply.github.com>
2024-09-30 13:24:46 +0100

    auth

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import routes from "./routes/index.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 
 app.use(cors("*"));
 app.use(express.json({ limit: "10mb" }));

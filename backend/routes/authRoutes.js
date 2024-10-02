d039c0c4c4953802b4323679ea50bbe38ffd7a76
temchelsy <148960005+temchelsy@users.noreply.github.com>
2024-10-02 13:45:03 +0100

    transaction route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { signinUser, signupUser } from "../controllers/authController.js";
+import { signinUser, signupUser,getCurrentUser } from "../controllers/authController.js";
+import authMiddleware from '../middleware/authMiddleware.js'; 
 
 const router = express.Router();
 
 router.post("/sign-up", signupUser);
 router.post("/sign-in", signinUser);
+router.get('/me', authMiddleware, getCurrentUser);
 
 export default router;
